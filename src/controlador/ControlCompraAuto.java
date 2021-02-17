d9096caf0fa4aa368870790a5baed58785b2777e
AriPaez <paezariel69@gmail.com>
2021-02-17 13:37:50 -0300

    compraAuto.comprarAutoViejo

--- src/controlador/ControlCompraAuto.java
+++ src/controlador/ControlCompraAuto.java
@@ -66,7 +66,7 @@ public class ControlCompraAuto extends WindowAdapter  implements ActionListener,
 		{
 			if(botonElegido==compraAutoViejo.getComprar())
 			{
-				 compraAuto.comprarAutoNuevo();
+				 compraAuto.comprarAutoViejo();
 			}
 			else if(botonElegido==compraAutoViejo.getCancelar())
 			{
